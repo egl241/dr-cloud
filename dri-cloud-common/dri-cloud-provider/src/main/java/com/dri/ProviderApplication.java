8397f0469cba3b276a4775bde8df7ace0e01b30d
egl241 <egl241@163.com>
2019-06-24 18:14:45 +0800

    fix配置中心

--- dri-cloud-common/dri-cloud-provider/src/main/java/com/dri/ProviderApplication.java
+++ dri-cloud-common/dri-cloud-provider/src/main/java/com/dri/ProviderApplication.java
@@ -6,11 +6,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProducerApplication {
+public class ProviderApplication {
 
 	public static void main(String[] args) {
 		
-		SpringApplication.run(ProducerApplication.class, args);
+		SpringApplication.run(ProviderApplication.class, args);
 	}
 
 }
